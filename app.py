839466a721a73365a7b2b8cd57d703a24625d930
BlueForced <cfbuddy124@gmail.com>
2024-03-23 21:11:56 +0200

    yeee

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from resources import (posts, post, post_replies, post_reply, auth, users, uploa
 
 
 def register_resources(curr_api):
-    curr_api.add_resource(posts.Posts, '/posts/<string:sort_column>/<int:page_number>/')
+    curr_api.add_resource(posts.Posts, '/posts/<string:sort_column>/<int:page_number>')
     curr_api.add_resource(search_posts.SearchPosts,
-                          '/search_posts/<string:search_query>/<string:sort_column>/<int:page_number>/')
+                          '/search_posts/<string:search_query>/<string:sort_column>/<int:page_number>')
     curr_api.add_resource(post.Post, '/post', '/post/<string:post_id>')
     curr_api.add_resource(post_replies.PostReplies, '/post_replies/<string:post_id>', '/post_replies/<string:post_id>/'
                           '<string:sort_column>/<int:page_number>')
@@ -16,8 +16,8 @@ def register_resources(curr_api):
     curr_api.add_resource(users.Users, '/users', '/users/<string:user_id>')
     curr_api.add_resource(upload.Upload, '/upload')
     curr_api.add_resource(refresh.Refresh, '/refresh')
-    curr_api.add_resource(post_reactions.PostReactions, '/post_reactions', '/post_reactions/<string:post_id>')
-    curr_api.add_resource(reply_reactions.ReplyReactions, '/reply_reactions', '/reply_reactions/<string:reply_id>')
+    curr_api.add_resource(post_reactions.PostReactions, '/post_reactions/<string:post_id>')
+    curr_api.add_resource(reply_reactions.ReplyReactions, '/reply_reactions/<string:reply_id>')
     curr_api.add_resource(reactions.Reactions, '/reactions')
 
 
